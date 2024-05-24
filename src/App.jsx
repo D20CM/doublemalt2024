441842bfabc78782900789cc147b93bb4044a231
D20CM <scottbrown500@hotmail.com>
2024-05-24 16:19:40 +0100

    initial basic responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <>
       <Homepage />
-      {/* <Videopage /> */}
-      {/* <About /> */}
-      {/* <Contact /> */}
+      <Videopage />
+      <About />
+      <Contact />
     </>
   );
 }
